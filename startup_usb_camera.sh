3a77c8d76fd940a914cef617bb81e2efbbfb6a2a
Richard-TX <richard-tx@users.noreply.github.com>
2024-04-04 23:28:12 -0500

    Create startup_usb_camera.sh

---
+++ startup_usb_camera.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#Thia file inits 3 cameras  total.  The built in camera and 2 usb cameras
+
+kill -9 $(pidof mjpg_streamer)
+kill -9 $(fuser /dev/video4)
+kill -9 $(fuser /dev/video5)
+kill -9 $(fuser /dev/video6)
+
+
+mjpg_streamer -b -i "/usr/lib/mjpg-streamer/input_uvc.so -d /dev/video4" \
+ -i "/usr/lib/mjpg-streamer/input_uvc.so -d /dev/video5" \
+ -i "/usr/lib/mjpg-streamer/input_uvc.so -d /dev/video6" \
+ -o "/usr/lib/mjpg-streamer/output_http.so -p 8080"
